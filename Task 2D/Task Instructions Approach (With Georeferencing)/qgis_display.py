aa856df8bffebac75456e915be1ace4c524556b6
Sumit Bhardwaj <95293951+smtbhd32@users.noreply.github.com>
2023-01-22 00:07:17 +0530

    Add files via upload

--- Task 2D/Task Instructions Approach (With Georeferencing)/qgis_display.py
+++ Task 2D/Task Instructions Approach (With Georeferencing)/qgis_display.py
@@ -1,36 +1,45 @@
-#!/usr/bin/env python3
+import os
+from qgis.core import QgsVectorLayer
+from tkinter import * 
 
-import rospy
-from sentinel_drone.msg import Geolocation
-from qgis.utils import *
-from qgis.core import *
-from  gdalconst import *
-from PyQt5.QtCore import QFileInfo,QSettings
-from qgis.core import QgsRasterLayer, QgsCoordinateReferenceSystem
+#location of task2d.tiff file
+file = "/home/missmessedup/catkin_ws/src/sentinel_drone/sentinel_drone/scripts/task2d.tif"
 
-def loc_plotter(Geolocation):
-    rospy.loginfo(Geolocation)
-    canvas = iface.mapCanvas() 
-    pnt = QgsPointXY(Geolocation.long, Geolocation.lat)
-    m = QgsVertexMarker(canvas)
-    m.setCenter(pnt)
-    m.setColor(QColor('255,0, 0'))
-    m.setIconType(QgsVertexMarker.ICON_X)
-    m.setIconSize(12)
-    m.setPenWidth(5)
-    m.setFillColor(QColor(0, 200, 0))
+#Opening OpenStreetMap if it's not opened
+sources = [layer.source() for layer in QgsProject.instance().mapLayers().values()]
+source_found = False
+for source in sources:
+    if 'xyz&url' in source:
+        source_found = True
+        print('!!!Open Street Map Already Loaded!!!')
+    if file in source:
+        file_found = True
+        print('!!!Task2D.tif Loaded!!!')
+if not source_found:
+    print('LOADING Open Street Map...')
+    urlWithParams = 'type=xyz&url=http://a.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&zmax=19&zmin=0&crs=EPSG4326'
+    rlayer = QgsRasterLayer(urlWithParams, 'OpenStreetMap', 'wms')
+    if rlayer.isValid():
+        QgsProject.instance().addMapLayer(rlayer)
+    else:
+        print('invalid layer')
 
-def Plot():
-    file = "/home/missmessedup/catkin_ws/src/sentinel_drone/sentinel_drone/scripts/task2d.tif"
-    fileInfo = QFileInfo(file)    
-    baseName = fileInfo.baseName()
-    layer = QgsRasterLayer(file, baseName)  
-    QgsProject.instance().addMapLayer(layer)
-    rospy.init_node('QGIS_Listener')
-    rospy.Subscriber('/geolocation', Geolocation, loc_plotter)
+#Specifying the extent of Arena using task_2d.tiff file
+fileInfo = QFileInfo(file)    
+baseName = fileInfo.baseName()
+layer = QgsRasterLayer(file, baseName)  
 
-Plot()
+#Attaching the .csv file, that gets updated with location when a new block is found...
+uri = r"file:///home/missmessedup/catkin_ws/src/sentinel_drone/sentinel_drone/scripts/plot.csv?type=csv&detectTypes=yes&xField={}&yField={}&crs=EPSG:4326&spatialIndex=no&subsetIndex=no&watchFile=yes".format("Longitude", "Latitude")
+while True:
+    vlayer = QgsVectorLayer(uri, "yellow_block", "delimitedtext")
+    if vlayer.isValid():
+        print("Loading layer...")
+        QgsProject.instance().addMapLayer(vlayer)
+        break
 
-
-
-
+print("!!!LAYER LOADED!!!")
+#Adjusting the extent of canvas
+canvas = iface.mapCanvas()
+canvas.setExtent(layer.extent())
+canvas.refresh()
